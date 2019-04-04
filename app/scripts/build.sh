88dab30ecfe6cb05aff76f04feacb1a10b845966
Nandi Wong <nandi0315@gmail.com>
2019-04-04 15:26:29 +0800

    update config

---
+++ app/scripts/build.sh
@@ -0,0 +1,3 @@
+docker build -t hongkong-map-timeline-frontend .
+docker tag hongkong-map-timeline-frontend nandiheath/hongkong-map-timeline-frontend:latest
+docker push nandiheath/hongkong-map-timeline-frontend:latest
