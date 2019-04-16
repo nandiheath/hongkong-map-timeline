4400b0be6aad96c67f2a7dd6e9c5c7688210991e
Nandiheath <nandi0315@gmail.com>
2019-04-16 23:17:44 +0800

    add nuxt files

--- app/next.config.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const withTypescript = require('@zeit/next-typescript');
-
-module.exports = Object.assign({
-  publicRuntimeConfig: { // Will be available on both server and client
-    API_HOST: process.env.API_HOST
-  }
-}, withTypescript());
---
+++ app/pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-layout>
+    <no-ssr>
+      <OLMap/>
+    </no-ssr>
+  </v-layout>
+</template>
+
+<script>
+let OLMap
+if (process.client) {
+  OLMap = require('~/components/OLMap.vue').default
+}
+
+export default {
+  components: {
+    OLMap
+  }
+}
+</script>
